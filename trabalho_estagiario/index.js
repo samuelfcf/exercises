cf54aff1271a9225b7983d23a98e7abb3f8f1f7a
Samuel Felipe <samuelfelipef@gmail.com>
2021-10-06 19:11:19 -0300

    update function

--- trabalho_estagiario/index.js
+++ trabalho_estagiario/index.js
@@ -1,5 +1,5 @@
 import fs from 'fs';
 
 for (let i = 0; i < 100; i++) {
-  fs.writeFileSync(`${i + 1}.txt`, `Lista ${i + 1} - Cálculo ${i + 1} - Exercício ${i + 1}`);
+  fs.writeFileSync(`${i + 1}.txt`, `Lista 1 - Cálculo 1 - Exercício ${i + 1}`);
 }
